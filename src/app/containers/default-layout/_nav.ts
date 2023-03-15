6985869aac8bc6ecdd8508713f590f23c91139ff
tafshirkhan <78077360+tafshirkhan@users.noreply.github.com>
2023-03-15 17:07:01 +0600

    Project dashboard updated

---
+++ src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,102 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Components',
+    title: true
+  },
+  {
+    name: 'Admin',
+    url: '/base',
+    iconComponent: { name: 'cil-puzzle' },
+    children: [
+      {
+        name: 'Train',
+        url: '/base/train-list'
+      },
+      {
+        name: 'Breadcrumbs',
+        url: '/base/breadcrumbs'
+      },
+      {
+        name: 'Cards',
+        url: '/base/cards'
+      },
+      {
+        name: 'Carousel',
+        url: '/base/carousel'
+      },
+      {
+        name: 'Collapse',
+        url: '/base/collapse'
+      },
+      {
+        name: 'List Group',
+        url: '/base/list-group'
+      },
+      {
+        name: 'Navs & Tabs',
+        url: '/base/navs'
+      },
+      {
+        name: 'Pagination',
+        url: '/base/pagination'
+      },
+      {
+        name: 'Placeholder',
+        url: '/base/placeholder'
+      },
+      {
+        name: 'Popovers',
+        url: '/base/popovers'
+      },
+      {
+        name: 'Progress',
+        url: '/base/progress'
+      },
+      {
+        name: 'Spinners',
+        url: '/base/spinners'
+      },
+      {
+        name: 'Tables',
+        url: '/base/tables'
+      },
+      {
+        name: 'Tabs',
+        url: '/base/tabs'
+      },
+      {
+        name: 'Tooltips',
+        url: '/base/tooltips'
+      }
+    ]
+  },
+  // {
+  //   title: true,
+  //   name: 'Extras'
+  // },
+  {
+    name: 'Pages',
+    url: '/login',
+    iconComponent: { name: 'cil-star' },
+    children: [
+      {
+        name: 'Login',
+        url: '/login'
+      },
+      {
+        name: 'Register',
+        url: '/register'
+      },
+      {
+        name: 'Error 404',
+        url: '/404'
+      },
+      {
+        name: 'Error 500',
+        url: '/500'
+      }
+    ]
+  },
+];
