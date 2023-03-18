ff04ae5227a9d2f5edda9a07652b9349e45e38da
tafshirkhan <78077360+tafshirkhan@users.noreply.github.com>
2023-03-19 02:22:38 +0600

    Login & registration added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const routes: Routes = [
   },
   {
     path: '',
-    component: LoginComponent,
+    component: DefaultLayoutComponent,
+    //component: LoginComponent,
     data: {
       title: 'Home'
     },
